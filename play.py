8607ad09e95bf2f2489569e31ad2d2e13b27fba3
Magnitik1 <alexalex23122004@gmail.com>
2024-01-16 12:47:18 +0100

    play changed

--- play.py
+++ play.py
@@ -2,6 +2,22 @@ import pygame
 import home
 import props
 
+def create_Select_level_button(screen, x, y):
+    screenSizeX, screenSizeY = screen.get_size()
+
+    global levelS
+    global levelBoxS
+    global levelL
+    global levelBoxL
+    imp = pygame.image.load(f".\\imgs\\level{x*y+x+1}.png")
+    levelS = pygame.transform.scale(imp, (screenSizeX / 6.5, screenSizeX / 6.5))
+    levelBoxS = levelS.get_rect()
+    levelBoxS.center = (screenSizeX / 4, screenSizeY / 1.85)
+    levelL = pygame.transform.scale(imp, (screenSizeX / 5.5, screenSizeX / 5.5))
+    levelBoxL = levelL.get_rect()
+    levelBoxL.center = (screenSizeX / 4, screenSizeY / 1.85)
+    screen.blit(levelS, levelBoxS)
+
 
 def play_button_pressed(screen, full_back_button):
     screenSizeX, screenSizeY = screen.get_size()
@@ -11,11 +27,8 @@ def play_button_pressed(screen, full_back_button):
     marTop = 4 * t
     screen.fill('purple')
     for i in range(2):
-        marLeft = 2 * t
         for j in range(3):
-            pygame.draw.rect(screen, (0, 0, 255), [marLeft, marTop, w - t, h - t], 0)
-            marLeft += w
-        marTop += h
+            create_Select_level_button(screen, j, i)
     screen.blit(full_back_button['backS'], full_back_button['backBoxS'])
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
